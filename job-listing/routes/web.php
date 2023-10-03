e5ac78b3fea3d0900eb26da05c850a705071ac62
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-10-03 21:29:25 +0200

    layout and views

--- job-listing/routes/web.php
+++ job-listing/routes/web.php
@@ -4,4 +4,15 @@ use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [Listing::class, 'all']);
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+Route::get('listings/{id}', function ($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
