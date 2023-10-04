732d33e84533c56304b86bcc9e0b9df4368b5c23
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-10-04 17:58:13 +0200

    upgrade scope filter for search bar

--- job-listing/app/Http/Controllers/ListingController.php
+++ job-listing/app/Http/Controllers/ListingController.php
@@ -12,7 +12,7 @@ class ListingController extends Controller
     {
 
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 
